6663061f38bfbec1b5457cc80a586b851994ccdf
David Shere <davidshere@alizarinarts.com>
2012-07-23 17:15:01 -0700

    first commit

---
+++ Message.hs
@@ -0,0 +1,42 @@
+module Message where
+import Safe
+
+data Message =
+    MessagePrefixed {
+        prefix  :: String,
+        command :: Command,
+        params  :: Parameters
+    }
+  | MessageUnprefixed {
+        command :: Command,
+        params  :: Parameters
+    }
+    deriving (Show)
+
+data Command = 
+    CommandNum Integer
+  | CommandStr String
+    deriving (Show, Eq)
+
+type Parameters = [String]
+
+parseMessage :: String -> Message
+parseMessage str =
+    case str of
+      (':':xs) -> MessagePrefixed
+                    (words str !! 0)
+                    (readCommand (words str !! 1))
+                    [unwords $ drop 2 (words str)]
+      str      -> MessageUnprefixed
+                    (readCommand (words str !! 0))
+                    [unwords $ drop 1 (words str)]
+
+readCommand :: String -> Command
+readCommand str =
+    case readMay str of
+      Nothing -> CommandStr str
+      Just n  -> CommandNum n
+
+messageCommand :: Message -> Command
+messageCommand (MessagePrefixed _ c _) = c
+messageCommand (MessageUnprefixed c _) = c
---
+++ main.hs
@@ -0,0 +1,88 @@
+import Data.List
+import Data.List.Split
+import Network
+import System.IO
+import System.Exit
+import System.Posix
+import Control.Arrow
+import Control.Monad.Reader
+import Control.OldException
+import Text.Printf
+import Prelude hiding (catch)
+
+import Message
+
+server = "irc.canternet.org"
+port   = 6667
+chan   = "#HackingIsMagic"
+nick   = "HayskellBot"
+
+-- The 'Net' monad, a wrapper over IO, carrying the bot's immutable state.
+type Net = ReaderT Bot IO
+data Bot = Bot { socket :: Handle }
+
+-- Setup actions to run on start and end, and run the main loop
+main :: IO ()
+main = bracket connect disconnect loop
+  where
+    disconnect = hClose . socket
+    loop st    = catch (runReaderT run st) (const $ return ())
+
+-- Connect to the server and return the initial bot state
+connect :: IO Bot
+connect = notify $ do
+    h <- connectTo server (PortNumber (fromIntegral port))
+    hSetBuffering h NoBuffering
+    return (Bot h)
+  where
+    notify a = bracket_
+        (printf "Connecting to %s ..." server >> hFlush stdout)
+        (putStrLn "done.")
+        a
+
+-- We're in the Net monad now, so we've connected successfully
+-- Join a channel, and start processing commands
+run :: Net ()
+run = do
+    write "NICK" nick
+    write "USER" (nick++" 0 * :hayskell bot")
+    asks socket >>= listen
+
+-- Process each line from the server
+listen :: Handle -> Net ()
+listen h = forever $ do
+    s <- init `fmap` io (hGetLine h)
+    io (putStrLn s)
+    let m = parseMessage s
+    if ping s then pong s
+    else if isConnected $ command m then joinChannel    -- Wait for MOTD before joining
+    else eval (clean s)
+  where
+    forever a   = a >> forever a
+    clean       = drop 1 . dropWhile(/= ':') . drop 1
+    ping x      = "PING :" `isPrefixOf` x
+    pong x      = write "PONG" (':' : drop 6 x)
+    isConnected = (==) $ CommandNum 1
+    joinChannel = write "JOIN" chan
+
+-- Dispatch a command
+eval :: String -> Net ()
+eval     "!quit"                 = write "QUIT" ":Exiting" >> io (exitWith ExitSuccess)
+eval x | "!echo " `isPrefixOf` x = privmsg (drop 4 x)
+eval _                           = return ()
+
+-- Send a privmsg to the server we're currently connected to
+privmsg :: String -> Net ()
+privmsg s = write "PRIVMSG" (chan ++ " :" ++ s)
+
+-- Send a message out to the server we're currently connected to
+write :: String -> String -> Net ()
+write s t = do
+    h <- asks socket
+    io $ hPrintf h "%s %s\r\n" s t
+    io $ printf    "> %s %s\n" s t
+
+
+-- Convenience.
+io :: IO a -> Net a
+io = liftIO
